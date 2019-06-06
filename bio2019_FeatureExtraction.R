d65a6504bcf3da1b0801e9884e392b4aaf0ad390
Raquel Aoki <23690490+raquelaoki@users.noreply.github.com>
2019-06-05 21:38:53 -0700

    Update bio2019_FeatureExtraction.R

--- bio2019_FeatureExtraction.R
+++ bio2019_FeatureExtraction.R
@@ -1,23 +1,29 @@
-#Deleting previous files
+#--------#--------#--------#--------#--------#--------#--------#--------#--------#
+#--------#--------#--------#--------#--------#--------#--------#--------#--------#
+# Author: Raquel Aoki
+# Data: 2019/06 
+#--------#--------#--------#--------#--------#--------#--------#--------#--------#
+#--------#--------#--------#--------#--------#--------#--------#--------#--------#
+
+
+#--------#--------#--------#--------#--------#--------#--------#--------#--------#
+# I: Preprossing the data 
+#--------#--------#--------#--------#--------#--------#--------#--------#--------#
+
+#-------- Cleaning the environment 
 rm(list=ls(all=T))
 
-#Loading the dataset
+
+#-------- Packages 
+require(Rfast) #Variance by column s
+require(ggplot2) #Plots
+
+
+#-------- Loading the dataset
+#RNASEQ data: each row is a gene and each column a patient 
+#probeset: gene ID
+#pid: patient id 
 load("Data/HTA20_RMA.RData")
-dim(eset_HTA20)
-  
-  
-#probeset is compoised of entrez-gene ID
-#pid has the patient id 
-probeset = rownames(eset_HTA20)
-pid = colnames(eset_HTA20)
-head(probeset,20)
-head(pid)
-
-
-#Transforming dataset: now rows are patients and columns are genes
-data = t(eset_HTA20)
-head(eset_HTA20[,c(1:30)])
-rm(eset_HTA20)
 
 #Reading sample annotation file 
 #SampleID: unique identifier of the sample (matching the name of the .CEL file in HTA20 folder, 
@@ -30,6 +36,28 @@ rm(eset_HTA20)
 sample = read.csv('Data/anoSC1_v11_nokey.csv', sep = ',', header=T)
 
 
+#--------  Checking datasets dimentions 
+dim(eset_HTA20)
+dim(sample)
+  
+
+#-------- Saving the genes IDS and Patient IDS  
+gid = rownames(eset_HTA20)
+pid = colnames(eset_HTA20)
+#Checking the ids 
+head(gid)
+head(pid)
+
+
+#-------- Transforming gene dataset: now rows are patients and columns are genes
+data = t(eset_HTA20)
+rm(eset_HTA20)
+
+
+#--------#--------#--------#--------#--------#--------#--------#--------#--------#
+# II: Feature Extraction  
+#--------#--------#--------#--------#--------#--------#--------#--------#--------#
+
 #Feature Extraction 
 #1) Removing genes with many missing values
 #There is no missing values
@@ -37,15 +65,24 @@ sample = read.csv('Data/anoSC1_v11_nokey.csv', sep = ',', header=T)
 #sum(na_count)
 
 #2) Removing genes with lowest variance 
-require(Rfast)
+
+
 #var_gene = colVars(data)
 eliminate = data.frame(col = c(1:dim(data)[2]),var_gene = colVars(data))
 head(eliminate)
 eliminate = subset(eliminate, var_gene<quantile(eliminate$var_gene,0.1))
+dim(data)
+data = subset(data, select = -c(eliminate$col))
+dim(data)
 
-da
 #3) PCA
+genes.pca <- prcomp(data, center = TRUE,scale. = TRUE)
+#https://www.datacamp.com/community/tutorials/pca-analysis-r
+vars <- colVars(genes.pca$x)  
+props <- cumsum(vars / sum(vars))
 
+
+plot(props, xlab = 'Components (1-735')
 
 
 #4) Random Forest 
